ea6f131d2b2ac7b1220ed2cb9d987fe71061437a
illuka6 <06eri0109@gmail.com>
2024-09-18 21:05:25 +0800

    動畫

---
+++ JS/index.js
@@ -0,0 +1,33 @@
+const colors = ['#ff1491', '#ff63b6', '#eae72d', '#cc24ff'];
+
+document.addEventListener('mousemove', (e) => {
+    const index = Math.floor((e.clientX / window.innerWidth) * (colors.length - 1));
+    const nextIndex = (index + 1) % colors.length;
+
+    document.querySelector('.line').style.background = `linear-gradient(135deg, ${colors[index]}, ${colors[nextIndex]})`;
+});
+
+  
+/* 滑鼠 */
+
+/* 票券動畫 */
+
+
+gsap.from('.tk_wrap',{
+    //視窗滾動控制
+    scrollTrigger: {
+      trigger: '.tk_box',
+      markers: false ,
+      start: 'top center',
+      end: 'top 20%',
+  
+      scrub: true, //動畫播放是否以視窗滾動播放
+  
+    },
+    
+    //動畫控制
+    
+    rotation: 45, //旋轉45度
+    yPercent: '-310'
+    // YPercent: '-200'
+  })
