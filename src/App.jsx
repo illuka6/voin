528ce1a5d8bebf78cbf2a72cd06d87112a300b5b
illuka6 <06eri0109@gmail.com>
2024-09-19 22:34:50 +0800

    提交

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Home from "./pages/Home"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <Home />
       <h1>hello word</h1>
       
     </>
