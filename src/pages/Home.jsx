3b2f09734998703d37b0b364e6861e22f6d7faff
illuka6 <06eri0109@gmail.com>
2024-09-21 06:36:55 +0800

    圖片源修正

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,9 +86,9 @@ const Home = () => {
 
             <header>
                 <nav>
-                    <a href="#" className="logo"><img src="/public/home_img/logo.svg" alt="" /></a>
+                    <a href="#" className="logo"><img src="home_img/logo.svg" alt="" /></a>
                     <div className="btn_menu_wrap">
-                        <div className="search_box"><img src="/public/home_img/icon_search.svg" /><input type="text" placeholder="搜尋 | 尋找包場" /></div>
+                        <div className="search_box"><img src="home_img/icon_search.svg" /><input type="text" placeholder="搜尋 | 尋找包場" /></div>
                         <div className="vote_btn"><a href="#">發起揪團投票</a></div>
                         <div className="menu"><img src="" alt="" /></div>
                     </div>
@@ -106,16 +106,16 @@ const Home = () => {
                 </svg>
             </section>
             <section id="banner">
-                <div className="title"><img src="/home_img/VOIN_Joy.svg" alt="" /></div>
+                <div className="title"><img src="home_img/VOIN_Joy.svg" alt="" /></div>
                 <div className="text">
                     <div>投出你想參加的場次</div>
                     <div>Vote for Tickets</div>
                 </div>
                 <div className="banner_content">
                     <div className="content_wrap">
-                        <div className="tk target"><img src="/public/home_img/tk.svg" alt="" /></div>
+                        <div className="tk target"><img src="home_img/tk.svg" alt="" /></div>
                         <div className="line"></div>
-                        <img id="star" className="star" src="/public/home_img/star.svg" alt="" />
+                        <img id="star" className="star" src="home_img/star.svg" alt="" />
                     </div>
                 </div>
             </section>
@@ -134,13 +134,13 @@ const Home = () => {
                             </div>
                         </div>
                         <div className="tk tk_md">
-                            <div className="img_box"><img src="/public/home_img/item1.jpg" alt="" /></div>
+                            <div className="img_box"><img src="home_img/item1.jpg" alt="" /></div>
                             <div className="content_box">
                                 <h1>異形全系列觀影會</h1>
                                 <div className="text">
                                     <p>想和喜歡異型系列的粉絲揪一場1979開始的全系列私人連映會</p>
                                 </div>
-                                <div className="pl"><img src="/public/home_img/seat.svg" alt="" />
+                                <div className="pl"><img src="home_img/seat.svg" alt="" />
                                     <p>8</p>
                                 </div>
                                 <div className="tag_wrap">
@@ -164,7 +164,7 @@ const Home = () => {
                                 <div className="date">02.19</div>
                                 <div className="wd">週六</div>
                                 <div className="time">01:00 PM</div>
-                                <div className="location"><img src="/public/home_img/location_on.svg" alt="" />台北市</div>
+                                <div className="location"><img src="home_img/location_on.svg" alt="" />台北市</div>
                             </div>
                         </div>
 
@@ -184,10 +184,10 @@ const Home = () => {
                             <div className="title_en title1">Initiate a Vote</div>
                             <div className="text_s">從意願調查到收款完整規劃, 不再擔心萬人+1 零人匯款</div>
                         </div>
-                        <div className="text1"><img src="/public/home_img/Begging-Hand-Coin-2--Streamline-Ultimate.svg" alt="" />
+                        <div className="text1"><img src="home_img/Begging-Hand-Coin-2--Streamline-Ultimate.svg" alt="" />
                             <div className="slogan">自動化確認收款</div>
                         </div>
-                        <div className="text2"><img src="/public/home_img/mail.svg" alt="" />
+                        <div className="text2"><img src="home_img/mail.svg" alt="" />
                             <div className="slogan">成團自動發信通知</div>
                         </div>
                     </div>
@@ -198,33 +198,33 @@ const Home = () => {
                             <div className="text_s">隨時發起投票，想揪什麼就揪！
                                 或許有其他人替你實現願望！</div>
                         </div>
-                        <div className="text1"><img src="/public/home_img/vote.svg" alt="" />建立投票尋找志同道合的夥伴</div>
-                        <div className="text2"><img src="/public/home_img/lead.svg" alt="" />自己開團 or 向同好發送開團腦波</div>
+                        <div className="text1"><img src="home_img/vote.svg" alt="" />建立投票尋找志同道合的夥伴</div>
+                        <div className="text2"><img src="home_img/lead.svg" alt="" />自己開團 or 向同好發送開團腦波</div>
                     </div>
 
                 </div>
                 <div className="type_banner">
                 <div className="container" onMouseMove={handleMouseMove}>
                     <div className="others " 
-                    onMouseEnter={(event) => handleMouseEnter('/public/home_img/other.jpg', event)}
+                    onMouseEnter={(event) => handleMouseEnter('home_img/other.jpg', event)}
                     onMouseLeave={handleMouseLeave}>
                         <div className="en">Others</div>
                         <div className="cn">其他</div>
                     </div>
                     <div className="reading_club"
-                    onMouseEnter={(event) => handleMouseEnter('/public/home_img/reading.jpg', event)}
+                    onMouseEnter={(event) => handleMouseEnter('home_img/reading.jpg', event)}
                     onMouseLeave={handleMouseLeave}>
                         <div className="en">Reading Club</div>
                         <div className="cn">讀書會</div>
                     </div>
                     <div className="tea_party"
-                    onMouseEnter={(event) => handleMouseEnter('/public/home_img/tea.jpg', event)}
+                    onMouseEnter={(event) => handleMouseEnter('home_img/tea.jpg', event)}
                     onMouseLeave={handleMouseLeave}>
                         <div className="en">Tea Party</div>
                         <div className="cn">茶會</div>
                     </div>
                     <div className="movie"
-                    onMouseEnter={(event) => handleMouseEnter('/public/home_img/movie.jpg', event)}
+                    onMouseEnter={(event) => handleMouseEnter('home_img/movie.jpg', event)}
                     onMouseLeave={handleMouseLeave}>
                         <div className="en">Movie</div>
                         <div className="cn">電影</div>
@@ -252,34 +252,34 @@ const Home = () => {
                     <div className="htk_box">
                         <div className="htk_img">
                             <div className="type_tag">Movie</div>
-                            <div className="img_wrap"><img src="/public/home_img/item_madmax.jpg" alt="" /></div>
+                            <div className="img_wrap"><img src="home_img/item_madmax.jpg" alt="" /></div>
                         </div>
                         <div className="htk_content">
                             <div className="e_title">MadMax憤怒道放映會</div>
                             <div className="e_date">2024.12.25</div>
-                            <div className="e_location"><img src="/public/home_img/location_on.svg" alt="" />台中市</div>
+                            <div className="e_location"><img src="home_img/location_on.svg" alt="" />台中市</div>
                         </div>
                     </div>
                     <div className="htk_box">
                         <div className="htk_img">
                             <div className="type_tag">Reading Club</div>
-                            <div className="img_wrap"><img src="/public/home_img/cs_men.jpg" alt="" /></div>
+                            <div className="img_wrap"><img src="home_img/cs_men.jpg" alt="" /></div>
                         </div>
                         <div className="htk_content">
                             <div className="e_title">鏈鋸人漫畫讀書分享會</div>
                             <div className="e_date">2024.3.25</div>
-                            <div className="e_location"><img src="/public/home_img/location_on.svg" alt="" />台中市</div>
+                            <div className="e_location"><img src="home_img/location_on.svg" alt="" />台中市</div>
                         </div>
                     </div>
                     <div className="htk_box">
                         <div className="htk_img">
                             <div className="type_tag">Movie</div>
-                            <div className="img_wrap"><img src="/public/home_img/item_xMen.jpg" alt="" /></div>
+                            <div className="img_wrap"><img src="home_img/item_xMen.jpg" alt="" /></div>
                         </div>
                         <div className="htk_content">
                             <div className="e_title">X戰警全系列電影馬拉松</div>
                             <div className="e_date">2024.11.25</div>
-                            <div className="e_location"><img src="/public/home_img/location_on.svg" alt="" />台北市</div>
+                            <div className="e_location"><img src="home_img/location_on.svg" alt="" />台北市</div>
                         </div>
                     </div>
                 </div>
@@ -293,16 +293,16 @@ const Home = () => {
             </section>
             <section id="pic_show">
                 <div id="show_box" ref={showBoxRef}>
-                    <img id="pic_line" src="/public/home_img/picline.png" alt="" />
+                    <img id="pic_line" src="home_img/picline.png" alt="" />
                 </div>
             </section>
             <section id="follow_news">
                 <div className="content">
                     <div className="half_circle">
                     </div>
-                    <div className="text"><img src="/public/home_img/follow_text.svg" alt="" /></div>
+                    <div className="text"><img src="home_img/follow_text.svg" alt="" /></div>
                     <div className="email_box">
-                        <img src="/public/home_img/mail_w.svg" alt="" />
+                        <img src="home_img/mail_w.svg" alt="" />
                         <input type="text" placeholder="追蹤最新開團消息" />
                     </div>
 
